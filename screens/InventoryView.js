8a75767cd885cef2570754febe3a42030b46e90d
Geshnay <np021@bucknell.edu>
2018-09-27 21:59:18 -0400

    Inventory view kinda works

---
+++ screens/InventoryView.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import * as firebase from 'firebase';
+import firebaseConfig from '../firebaseConfig';
+
+firebase.initializeApp(firebaseConfig);
+
+
+
+export default class InventoryView extends React.Component {
+	
+	constructor() {
+    super();
+    this.tasksRef = firebase.database().ref("Inventory");
+    const dataSource = new ListView.DataSource({
+      rowHasChanged: (row1, row2) => row1 !== row2,
+    });
+    this.state = {
+      dataSource: dataSource
+    };
+  }
+
+  listenForTasks(tasksRef) {
+    tasksRef.on('value', (dataSnapshot) => {
+      var tasks = [];
+      dataSnapshot.forEach((child) => {
+        //console.log(child.key)
+        console.log(child.val().name)
+        tasks.push({
+          name: child.val().name
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(tasks)
+      });
+    });
+  }
+
+  componentDidMount() {
+    // start listening for firebase updates
+    this.listenForTasks(this.tasksRef);
+  }
+
+
+
+  static navigationOptions = {
+    headerTitle: "List View"
+  }
+
+  render() {
+    return (
+      <View style = {styles.container}>
+        <Button
+          title="List View"
+          onPress={
+            () => this.props.navigation.navigate("Login")
+          }
+        />
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(data) => <Row {...data} />}
+          enableEmptySections={true}
+        />
+      </View>
+      );
+
+  }
+}
+
+const Row = (props) => (
+  <View style={styles.container}>
+    <Text style={styles.text}>
+      {`${props.name}`}
+    </Text>
+  </View>
+);
+
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      backgroundColor: '#B6A6BB',
+    }
+  });
