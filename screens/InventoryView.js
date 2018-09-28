4e3d75c0762d7e4985d15cc99a09d2b2a0c8ca51
Geshnay <np021@bucknell.edu>
2018-09-28 15:17:20 -0400

    Updating inventory view

--- screens/InventoryView.js
+++ screens/InventoryView.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, ListView } from 'react-native';
 
 import * as firebase from 'firebase';
-import firebaseConfig from '../firebaseConfig';
+import firebaseConfig from 'firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
-
-
 
 export default class InventoryView extends React.Component {
 	
 	constructor() {
     super();
-    this.tasksRef = firebase.database().ref("Inventory");
+    this.tasksRef = firebase.database().ref("Inventory/Lewisburg Pantry");  // change the name of the pantry with what the user selects before.
     const dataSource = new ListView.DataSource({
       rowHasChanged: (row1, row2) => row1 !== row2,
     });
@@ -21,21 +19,25 @@ export default class InventoryView extends React.Component {
     };
   }
 
+
   listenForTasks(tasksRef) {
     tasksRef.on('value', (dataSnapshot) => {
+      //console.log(dataSnapshot.val());
       var tasks = [];
       dataSnapshot.forEach((child) => {
-        //console.log(child.key)
-        console.log(child.val().name)
+        //console.log(child.val());
         tasks.push({
-          name: child.val().name
+          item_name: child.key,
+          item_quantity: child.val(),
         });
-      });
+      })
 
       this.setState({
         dataSource: this.state.dataSource.cloneWithRows(tasks)
       });
+      
     });
+    
   }
 
   componentDidMount() {
@@ -43,20 +45,18 @@ export default class InventoryView extends React.Component {
     this.listenForTasks(this.tasksRef);
   }
 
-
-
   static navigationOptions = {
-    headerTitle: "List View"
+    headerTitle: "Inventory View"
   }
 
   render() {
     return (
       <View style = {styles.container}>
         <Button
-          title="List View"
-          onPress={
-            () => this.props.navigation.navigate("Login")
-          }
+          title="Inventory View"
+          //onPress={
+           // () => this.props.navigation.navigate("Login")
+         // }
         />
         <ListView
           dataSource={this.state.dataSource}
@@ -71,18 +71,30 @@ export default class InventoryView extends React.Component {
 
 const Row = (props) => (
   <View style={styles.container}>
-    <Text style={styles.text}>
-      {`${props.name}`}
+    <Text>
+      <Text style={styles.text}> {`${props.item_name}`} </Text>
+      <Text style = {{textAlign: 'right'}}>  {`${props.item_quantity}`}  </Text>
     </Text>
   </View>
 );
 
-
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      backgroundColor: '#B6A6BB',
-    }
-  });
+  container: {
+    justifyContent: "center",
+    flex: 1
+  },
+  text: {
+    textAlign: "center"
+  }
+});
+
+
+
+    //*/ 
+    /*0
+    <View style={{ flex: 1, alignSelf: 'stretch', flexDirection: 'row' }}>
+      <View style={{ flex: 1, alignSelf: 'stretch' }}>
+        <Text>{`${props.item_name}`}</Text>
+      </View>
+    </View>
+    */ 
