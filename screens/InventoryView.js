6fb0266d70fc1581e00506df69fbe6d80ec60f85
Geshnay <np021@bucknell.edu>
2018-10-05 15:44:47 -0400

    Inventory view edit and done button works

--- screens/InventoryView.js
+++ screens/InventoryView.js
@@ -1,51 +1,65 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, ListView, TouchableOpacity } from 'react-native';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ListView,
+  TouchableHighlight,
+  TouchableOpacity,
+} from 'react-native';
+import { Constants } from 'expo';
 import * as firebase from 'firebase';
-import firebaseConfig from '../firebaseConfig';
+import firebaseConfig from 'firebaseConfig';
 
 //firebase.initializeApp(firebaseConfig);
 
 export default class InventoryView extends React.Component {
-
-
-	constructor(props) {
+  constructor(props) {
     super(props);
-    name = this.props.navigation.getParam("name", "NO-name");
-    name = "Inventory/" + name
+    this.tasksRef = firebase.database().ref('Inventory/Lewisburg Pantry');
 
-
-    this.tasksRef = firebase.database().ref(name);  // change the name of the pantry with what the user selects before.
     const dataSource = new ListView.DataSource({
       rowHasChanged: (row1, row2) => row1 !== row2,
     });
     this.state = {
-      dataSource: dataSource
+      dataSource: dataSource,
+      editItem: false,
     };
   }
 
-  static navigationOptions = {
-    headerTitle: "Inventory View"
-  }
+  toggleEdit = () => {
+    this.setState(prevState => ({ editItem: !prevState.editItem }));
+  };
 
   listenForTasks(tasksRef) {
-    tasksRef.on('value', (dataSnapshot) => {
+    tasksRef.on('value', dataSnapshot => {
       //console.log(dataSnapshot.val());
       var tasks = [];
-      dataSnapshot.forEach((child) => {
+      dataSnapshot.forEach(child => {
         //console.log(child.val());
         tasks.push({
           item_name: child.key,
           item_quantity: child.val(),
         });
-      })
+      });
+      console.log(tasks[1]);
 
       this.setState({
-        dataSource: this.state.dataSource.cloneWithRows(tasks)
+        dataSource: this.state.dataSource.cloneWithRows(tasks),
       });
-      
     });
-    
+  }
+
+  renderConditionalText() {
+    if (this.state.editItem) {
+      return <Text> Done </Text>;
+    }
+    return <Text> Edit</Text>;
+  }
+
+  decreaseItemCount(item){
+    item.item_quantity--;
   }
 
   componentDidMount() {
@@ -53,50 +67,121 @@ export default class InventoryView extends React.Component {
     this.listenForTasks(this.tasksRef);
   }
 
-
   render() {
     return (
-      <View style = {styles.appContainer}>
-        
+      <View style={styles.appContainer}>
+        <TouchableOpacity style={{ paddingVertical: 10 }}>
+          <Text
+            style={{ textAlign: 'right', fontSize: 20, fontWeight: 'bold' }}
+            onPress={this.toggleEdit}>
+            {this.renderConditionalText()}
+          </Text>
+        </TouchableOpacity>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignSelf: 'center',
+          }}>
+          <Text style={styles.topText}>Item Name</Text>
+          <Text style={styles.topText}>Quantity</Text>
+        </View>
+
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={(data) => <Row {...data} />}
+          renderRow={data => (
+            <View>
+              <View style={styles.row}>
+                <Text style={styles.nameText}>{`${data.item_name}`}</Text>
+                <View style={styles.quantityContainer}>
+                  {this.state.editItem && (
+                    <TouchableHighlight
+                      style={styles.button}
+                      onPress={() => this.decreaseItemCount(data)} underlayColor="#dddddd">
+                      <Text style={styles.btnText}> - </Text>
+                    </TouchableHighlight>
+                  )}
+
+                  <Text style={styles.quantityText}>{`${
+                    data.item_quantity
+                  }`}</Text>
+
+                  {this.state.editItem && (
+                    <TouchableHighlight
+                      style={styles.button}
+                      /* onPress={() => this.addTodo()}*/ underlayColor="#dddddd">
+                      <Text style={styles.btnText}> + </Text>
+                    </TouchableHighlight>
+                  )}
+                </View>
+              </View>
+              <View style={styles.separator} />
+            </View>
+          )}
           enableEmptySections={true}
         />
       </View>
-      );
-
+    );
   }
 }
+
 
 const Row = props => (
   <View>
     <View style={styles.row}>
-        <Text style={styles.nameText}>{`${props.item_name}`}</Text>
+      <Text style={styles.nameText}>{`${props.item_name}`}</Text>
+      <View style={styles.quantityContainer}>
+        {this.state.editItem && (
+          <TouchableHighlight
+            style={styles.button}
+            /* onPress={() => this.addTodo()}*/ underlayColor="#dddddd">
+            <Text style={styles.btnText}> - </Text>
+          </TouchableHighlight>
+        )}
+
         <Text style={styles.quantityText}>{`${props.item_quantity}`}</Text>
+
+        <TouchableHighlight
+          style={styles.button}
+          /* onPress={() => this.addTodo()}*/ underlayColor="#dddddd">
+          <Text style={styles.btnText}> + </Text>
+        </TouchableHighlight>
+      </View>
     </View>
     <View style={styles.separator} />
   </View>
 );
 
+
 const styles = StyleSheet.create({
   appContainer: {
     flex: 1,
-    paddingTop: 20,
+    paddingTop: Constants.statusBarHeight,
+  },
+  topText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingHorizontal: 50,
+    paddingVertical: 5,
+    color: 'maroon',
+    textAlign: 'center',
   },
   nameText: {
     flex: 1,
     textAlign: 'left',
     fontSize: 20,
-    fontWeight: 'bold',
-    //paddingLeft: 20,
-    //color: 'red',
   },
   quantityText: {
     flex: 1,
-    textAlign: 'right',
+    textAlign: 'center',
     fontSize: 20,
-    paddingRight: 10,
+  },
+  quantityContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    fontSize: 20,
   },
   row: {
     flexDirection: 'row',
@@ -106,5 +191,19 @@ const styles = StyleSheet.create({
   separator: {
     height: 1,
     backgroundColor: '#CCCCCC',
+  },
+  button: {
+    height: 36,
+    flex: 0.4,
+    flexDirection: 'row',
+    backgroundColor: '#48afdb',
+    justifyContent: 'center',
+    color: '#FFFFFF',
+    borderRadius: 10,
+  },
+  btnText: {
+    fontSize: 20,
+    color: '#fff',
+    marginTop: 3,
   },
 });
