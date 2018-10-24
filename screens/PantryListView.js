2fe0db7bbc9fbae8fe0d25234716d3dbe8c0b468
Geshnay <np021@bucknell.edu>
2018-10-24 16:32:06 -0400

    Revert "login Nav"
    This reverts commit aa02d217c2e13e1217c6d5006ffe52225850ef32.

--- screens/PantryListView.js
+++ screens/PantryListView.js
@@ -86,6 +86,7 @@ export default class PantryListView extends React.Component {
                     })
                   }}>
                   <Text style = {styles.text} numberOfLines="1" ellipsizeMode="tail">{`${data.name}`}</Text>
+
                 </Button>
                 <Right>
                   <TouchableOpacity
