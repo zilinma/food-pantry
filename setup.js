9253bff19bc967fc36ed77a9037f3202744e6f31
zilinma <zm003@bucknell.edu>
2018-10-28 10:51:09 -0400

    push

---
+++ setup.js
@@ -0,0 +1,37 @@
+import * as Expo from "expo";
+import React, { Component } from "react";
+import { StyleProvider } from "native-base";
+
+//import App from "../App";
+//import getTheme from "../theme/components";
+//import variables from "../theme/variables/commonColor";
+
+import AppNavigator from './Navigators/AppNavigator';
+export default class Setup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
+  componentWillMount() {
+    this.loadFonts();
+  }
+  async loadFonts() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+      Georgia: require("./Georgia.ttf"),
+    });
+    this.setState({ isReady: true });
+  }
+  render() {
+    if (!this.state.isReady) {
+      return <Expo.AppLoading />;
+    }
+    return (
+        <AppNavigator/>
+    );
+  }
+}
